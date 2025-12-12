7435245fd3a183f4377764c9b44045e8388975bf
Anastasiya Simonenko <Simonenkoa2005@gmail.com>
2025-12-12 23:58:20 +0300

    Add main.py with argparse and main logic

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+from weather.parser import get_parser
+from weather.commands import process_command
+
+
+def main() :
+    parser = get_parser()
+    args = parser.parse_args()
+
+    try :
+        data = process_command(args)
+        print(f"Температура: {data['temperature']}°C, Скорость ветра: {data['windspeed']} км/ч")
+    except Exception as e :
+        print(f"Ошибка: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__" :
+    main()
+    
