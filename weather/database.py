e9c3e9e9639a7408b671136dc7f697959c09019d
Anastasiya Simonenko <Simonenkoa2005@gmail.com>
2025-12-13 04:05:36 +0300

    Adding explanations

--- weather/database.py
+++ weather/database.py
@@ -4,8 +4,8 @@ from typing import Optional
 DB_NAME = "weather_history.db"
 
 def init_db():
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
+    conn = sqlite3.connect(DB_NAME)     # Подключаемся к БД (если файла нет — создаётся)
+    cursor = conn.cursor()      # Курсор для выполнения SQL-запросов
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS requests (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,16 +17,16 @@ def init_db():
             windspeed REAL
         )
     ''')
-    conn.commit()
+    conn.commit()    # Сохраняем изменения
     conn.close()
 
-def log_to_db(city: Optional[str], lat: float, lon: float, temp: float, wind: float):
-    init_db()
+def log_to_db(city: Optional[str], lat: float, lon: float, temp: float, wind: float):      # Сохраняет результат запроса в БД
+    init_db()     # Сначала проверяем/создаём таблицу
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
     cursor.execute('''
         INSERT INTO requests (city, latitude, longitude, temperature, windspeed)
         VALUES (?, ?, ?, ?, ?)
-    ''', (city or "", lat, lon, temp, wind))
+    ''', (city or "", lat, lon, temp, wind))     # Параметры для защиты от SQL-инъекций
     conn.commit()
     conn.close()
