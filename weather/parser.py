2ed326544232b83a9e0e586a045f34c3b6bc4ef7
Anastasiya Simonenko <Simonenkoa2005@gmail.com>
2025-12-15 18:13:21 +0300

    parser docstrings

--- weather/parser.py
+++ weather/parser.py
@@ -1,7 +1,17 @@
+"""Модуль для обработки аргументов командной строки.
+
+Зачем нужен: Разбирает входные аргументы с помощью argparse для удобного запуска.
+Поддерживает --city или --coords.
+"""
 import argparse
 # Парсер нужен, чтобы программа удобно запускалась из консоли с разными параметрами
 
 def get_parser():
+    """Создаёт объект argparse.Parser для разбора аргументов.
+
+        Returns:
+            argparse.ArgumentParser: Готовый парсер с группой аргументов.
+        """
     parser = argparse.ArgumentParser(description='Погода по городу или координатам')
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('--city', type=str, help='Название города (например, Москва)')
